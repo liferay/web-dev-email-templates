ee242c2469dffca39590db04921ee0842d14e892
Phillip_Chan <phillip.chan@liferay.COM>
2016-08-29 14:30:39 -0700

    Inliner and tempating experimentation

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	// email settings
+	emailBodyWidth: '600px',
+	emailBodyPadding: '15px',
+
+	// styles
+	bodyColor: '#fff',
+	backgroundColor: '#f2f2f2',
+	primaryFont: 'Helvetica, Arial',
+	primaryFontColor: '#444444',
+	primaryFontSize: '15px',
+	secondaryFontSize: '12px',
+	secondaryFontColor: '#999999',
+	primaryAccentColor: '#3574E3'
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 'use strict';
 
-const gulp = require('gulp');
-const runSequence = require('run-sequence');
-const wrap = require('gulp-wrap');
-const rename = require('gulp-rename');
-const htmlPrettify = require('gulp-html-prettify');
 const argv = require('yargs').argv;
+const config = require('./config');
+const dust = require('gulp-dust');
 const file = require('gulp-file');
+const gulp = require('gulp');
+const htmlPrettify = require('gulp-html-prettify');
+const inlineCss = require('gulp-inline-css');
+const litmus = require('gulp-litmus');
 const livereload = require('gulp-livereload');
+const open = require('gulp-open');
+const rename = require('gulp-rename');
+const runSequence = require('run-sequence');
+const template = require('gulp-template');
+const wrap = require('gulp-wrap');
 
 // wrap components in base styles
 gulp.task('component-update', function() {
@@ -21,6 +27,33 @@ gulp.task('component-update', function() {
 	}));
 });
 
+/*
+	Base Tasks
+*/
+
+// setting up base
+gulp.task('base-styles', function() {
+	return gulp.src('src/base/src.css', { base: "./" })
+		.pipe(template(config))
+		.pipe(rename({basename: 'base'}))
+        .pipe(gulp.dest('.'))
+})
+
+gulp.task('base-inline', function() {
+	return gulp.src('src/base/src.html', { base: "./" })
+		.pipe(inlineCss())
+		.pipe(rename({basename: 'base'}))
+		.pipe(gulp.dest('.'))
+})
+
+gulp.task('base', function() {
+	runSequence('base-styles', 'base-inline');
+})
+
+/*
+	Component Tasks
+*/
+
 // creating new components - NOT DONE
 gulp.task('new-component', function() {
 	console.log(argv.name);
@@ -29,15 +62,53 @@ gulp.task('new-component', function() {
 		.pipe(gulp.dest(`src/components/${argv.name}/`));
 });
 
+gulp.task('inline', function() {
+	return gulp.src('src/components/button/src.html')
+		.pipe(inlineCss())
+		.pipe(gulp.dest('.'))
+});
+
+// litmus testing
+var litmusConfig = {
+    username: 'phillip.chan1@gmail.com',
+    password: 'thebible',
+    url: 'https://phillipchan11.litmus.com',
+    applications: [
+        'applemail6',
+        'gmailnew',
+        'ffgmailnew',
+        'chromegmailnew',
+        'iphone4s',
+    ]
+}
+
+gulp.task('litmus', function () {
+    return gulp.src('components/button/test.html')
+        .pipe(litmus(litmusConfig))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('openLitmus', function() {
+	gulp.src('index.html')
+  	.pipe(open({uri: 'https://litmus.com/builder'}));
+});
+
 // watching for file changes
 gulp.task('watch', function() {
-	gulp.watch('src/**/src.html', ['component-update']);
+	gulp.watch('src/**/src.html', function(callback) {
+		runSequence(
+			'base-styles',
+			'component-update'
+		)
+	} ['component-update']);
 });
 
 // our default task
 gulp.task('default', function(callback) {
 	runSequence(
+		'base',
 		'component-update',
+		'openLitmus',
 		'watch'
 	)
 });
